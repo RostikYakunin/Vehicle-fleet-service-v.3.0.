539727dfaf5aa8ca29e6463d5e5c231af59706ed
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2023-03-14 22:24:18 +0200

    create: created class DriverDtoHandler for handling incoming driverDto objects

---
+++ src/main/java/com/vehicle/api/dto/handler/DriverDtoHandler.java
@@ -0,0 +1,55 @@
+package com.vehicle.api.dto.handler;
+
+import com.vehicle.api.dto.DriverDto;
+import com.vehicle.api.models.drivers.Driver;
+import com.vehicle.api.models.drivers.DriverQualificationEnum;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Slf4j
+public class DriverDtoHandler {
+
+    public static Driver mappingDtoToDriverMethodAdd(DriverDto driverDto) {
+        Driver driver = new Driver();
+        driver.setNameOfDriver(driverDto.getNameOfDriver());
+        driver.setSurnameOfDriver(driverDto.getSurnameOfDriver());
+        driver.setPhoneNumber(driverDto.getNumberOfPhone());
+        driver.setQualificationEnum(
+                DriverQualificationEnum.valueOf(driverDto.getQualificationEnum().toUpperCase() + "_DRIVER")
+        );
+        log.info("New driver was mapped: " + driver);
+        return driver;
+    }
+
+    public static Driver mappingDtoToDriverMethodUpdate(DriverDto driverDto, Optional<Driver> driver) {
+        if (driver.isEmpty()) {
+            log.error("Error, driver with id = " + driverDto.getId() + " not found");
+            throw new RuntimeException("Error, driver with id = " + driverDto.getId() + " not found");
+        }
+
+        if (driverDto.getNameOfDriver() != null) {
+            log.info("Driver`s name was updated from " + driver.get().getNameOfDriver() + " to " + driverDto.getNameOfDriver());
+            driver.get().setNameOfDriver(driverDto.getNameOfDriver());
+        }
+
+        if (driverDto.getSurnameOfDriver() != null) {
+            log.info("Driver`s surname was updated from " + driver.get().getSurnameOfDriver() + " to " + driverDto.getSurnameOfDriver());
+            driver.get().setSurnameOfDriver(driverDto.getSurnameOfDriver());
+        }
+
+        if (driverDto.getNumberOfPhone() != null) {
+            log.info("Driver`s phone was updated from " + driver.get().getPhoneNumber() + " to " + driverDto.getNumberOfPhone());
+            driver.get().setPhoneNumber(driverDto.getNumberOfPhone());
+        }
+
+        if (driverDto.getQualificationEnum() != null) {
+            log.info("Driver`s name was updated from " + driver.get().getQualificationEnum() + " to " + driverDto.getQualificationEnum());
+            driver.get().setQualificationEnum(DriverQualificationEnum.valueOf(driverDto.getQualificationEnum().toUpperCase() + "_DRIVER"));
+        }
+
+        return driver.get();
+    }
+
+}
